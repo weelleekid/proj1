2f211bee78ae7cbf7acbb47991d706ea119017e0
highkim0922 <shineest96@g.skku.edu>
2020-11-29 17:52:50 +0900

    APPLICATION LOCATIONS 수정

--- src/main/java/com/project/sample/Application.java
+++ src/main/java/com/project/sample/Application.java
@@ -11,7 +11,7 @@ public class Application {
 	
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "/home/ubuntu/app/config/spring/real-application.yml";
+			+ "/app/config/spring/real-application.yml";
 	
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class)
